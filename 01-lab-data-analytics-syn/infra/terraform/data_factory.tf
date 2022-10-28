0288d5e56cd3e39c1efebcbbe7262c6df1e6160f
murggu <amurguzur@gmail.com>
2022-10-29 00:52:41 +0200

    fix D

--- 01-lab-data-analytics-syn/infra/terraform/data_factory.tf
+++ 01-lab-data-analytics-syn/infra/terraform/data_factory.tf
@@ -8,8 +8,8 @@ module "data_factory" {
   location = module.resource_group.location
 
   subnet_id                   = module.subnet_default[0].id
-  private_dns_zone_ids_df     = [module.private_dns_zones.list["privatelink.datafactory.azure.net"][0].id]
-  private_dns_zone_ids_portal = [module.private_dns_zones.list["privatelink.adf.azure.com"][0].id]
+  private_dns_zone_ids_df     = [module.private_dns_zones[0].list["privatelink.datafactory.azure.net"].id]
+  private_dns_zone_ids_portal = [module.private_dns_zones[0].list["privatelink.adf.azure.com"].id]
 
   module_enabled = var.enable_data_factory
   is_sec_module  = var.is_sec_enabled

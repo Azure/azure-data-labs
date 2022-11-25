960f025967542c72315fffb6ece8192314474771
jorgedocampo <jorge.docampo@outlook.com>
2022-11-25 13:14:29 +0100

    kv & eh
    added key vault and event hub to the template

--- 03-lab-data-analytics-databricks/infra/terraform/event_hubs.tf
+++ 03-lab-data-analytics-databricks/infra/terraform/event_hubs.tf
@@ -8,7 +8,7 @@ module "event_hubs_namespace" {
   subnet_id            = var.enable_private_endpoints ? module.subnet_default[0].id : null
   private_dns_zone_ids = var.enable_private_endpoints ? [module.private_dns_zones[0].list["privatelink.servicebus.windows.net"].id] : null
 
-  module_enabled = false
+  module_enabled = var.enable_event_hub_namespace
   is_sec_module  = var.enable_private_endpoints
 
   tags = local.tags
